14b186b70f55bcda458e8536869bb09c9622a662
Nemanja Perunicic <nemanja.perun98@gmail.com>
2024-04-12 09:37:41 +0200

    SMS done

--- positive_crew.py
+++ positive_crew.py
@@ -244,16 +244,19 @@ def extract_MS_365(text):
     # Define a regex pattern to capture everything between the specified phrases
     pattern1 = re.compile(r"rešenjem mesečno po korisniku\.(.*?)Cena usluge na mesečnom nivou", re.S)
     pattern2 = re.compile(r"Usluga podrazumeva\:(.*?)Cena usluge na mesečnom nivou", re.S)
+    pattern3 = re.compile(r"Komponente usluge\:(.*?)Cena usluge na mesečnom nivou", re.S)
 
     # Search for the pattern in the text
     match1 = pattern1.search(text)
     match2 = pattern2.search(text)
-
+    match3 = pattern3.search(text)
     if match1:
         # Return the captured block of text, stripping leading/trailing whitespace
         return match1.group(1).strip()
     elif match2:
         return match2.group(1).strip()
+    elif match3:
+        return match3.group(1).strip()
     else:
         return "Text not found between the specified phrases."
 
@@ -566,7 +569,124 @@ def replace_content_ESS(string_without_empty_lines):
     doc.save(docx_path.replace('.docx', '_modified.docx'))
 
 
+def replace_content_SMS(string_without_empty_lines):
+    multi_line_address = extract_narucilac(st.session_state.uploaded_file_content)
+    ime_firme = multi_line_address.splitlines()[0]
+    director_name = extract_direktor(st.session_state.uploaded_file_content)
 
+    # Format the address
+    formatted_address = format_address(multi_line_address, director_name)
+
+    docx_path = r'C:\Users\nemanja.perunicic\OneDrive - Positive doo\Desktop\allIn1\crewai\ugovor SMS.docx'
+    old_text_start = "POSLOVNO IME"  # A unique part of the paragraph to be replaced
+
+    replacements = [extract_broj_ponude(st.session_state.uploaded_file_content), extract_datum(st.session_state.uploaded_file_content)]
+
+    broj_meseci = extract_broj_meseci(st.session_state.uploaded_file_content)
+
+    doc = Document(docx_path)
+    for paragraph in doc.paragraphs:
+        if old_text_start in paragraph.text:
+            # Clear existing paragraph
+            for run in paragraph.runs:
+                run.clear()
+            # Insert new text
+            new_run = paragraph.add_run()  # Create a new run
+            # Add the bold part
+            new_run.text = ime_firme
+            new_run.bold = True
+            # Add the rest of the address in a separate run
+            rest_of_address = formatted_address[len(ime_firme):]
+            paragraph.add_run(rest_of_address)
+            break
+    
+    placeholder_pattern = re.compile(r"_{8,}")  # Matches 8 or more underscores
+    for paragraph in doc.paragraphs:
+        placeholder_pattern = re.compile(r"_{8,}")  # Matches 8 or more underscores
+        if "specifikaciji navedenoj u Ponudi Davaoca usluga" in paragraph.text:
+            # Replace the placeholders sequentially
+            original_text = paragraph.text
+            for replacement in replacements:
+                original_text = placeholder_pattern.sub(replacement, original_text, 1)  # Replace one at a time
+            paragraph.text = original_text
+            break
+
+    for paragraph in doc.paragraphs:
+        meseci_pattern = re.compile(r"\d+\s*meseci")
+        if meseci_pattern.search(paragraph.text):
+            updated_text = meseci_pattern.sub(f"{broj_meseci} meseci", paragraph.text)
+            paragraph.text = updated_text
+
+    string_without_empty_lines = string_without_empty_lines.split("\n")
+    if "Essentials" in string_without_empty_lines[0]:
+        for paragraph in doc.paragraphs:
+            if "5,00 €" and "6,50 €" in paragraph.text:
+                remove_paragraph(paragraph)
+    elif "Advanced" in string_without_empty_lines[0]:
+        for paragraph in doc.paragraphs:
+            if "2,50 €" and "3,00 €" in paragraph.text:
+                remove_paragraph(paragraph)
+    if "Server" not in string_without_empty_lines[1]:
+        for paragraph in doc.paragraphs:
+            if "8,50 €" and "10,20 €" in paragraph.text:
+                remove_paragraph(paragraph)
+            elif "Obim pružanja usluge zavisi od broja klijentskih korisnika i servera" in paragraph.text:
+                paragraph.text = re.sub(r"i servera", "", paragraph.text).strip()
+    else:
+        for paragraph in doc.paragraphs:
+            if "8,50 €" and "10,20 €" in paragraph.text:
+                paragraph.text = re.sub(paragraph.text, paragraph.text, paragraph.text).strip()
+
+
+    tip_podrske = extract_tip_podrske(st.session_state.uploaded_file_content)
+    # Assuming 'doc.paragraphs' and 'broj_meseci' are defined
+    for paragraph in doc.paragraphs:
+        # Check if the paragraph contains the simplified identification text
+        if "Pick 24 ili 36" in paragraph.text:
+            # Determine the replacement based on 'broj_meseci'
+            godina_replacement = "prve" if broj_meseci == "24" else "druge"
+            # Replace "prve/druge" with the determined replacement
+            paragraph.text = re.sub(r"\bprve/druge\b", godina_replacement, paragraph.text)
+            # Remove the simplified identification text
+            paragraph.text = re.sub(r"Pick 24 ili 36", "", paragraph.text).strip()
+
+    # Ensure to recheck the paragraph's text to confirm the changes are applied as expected.
+        if "Pick 24/7 ili 8/5" in paragraph.text:
+            # Determine the replacement based on 'broj_meseci'
+            dan_replacement = "jednog radnog" if tip_podrske == "24/7" else "dva radna"
+            # Replace "prve/druge" with the determined replacement
+            paragraph.text = re.sub(r"\bjednog radnog /dva radna\b", dan_replacement, paragraph.text)
+            # Remove the simplified identification text
+            paragraph.text = re.sub(r"Pick 24/7 ili 8/5", "", paragraph.text).strip()
+
+        if "Obezbeđivanje podrške – 24/7 ili 8/5" in paragraph.text:
+            paragraph.text = re.sub(r"\b24/7 ili 8/5\b", tip_podrske, paragraph.text)
+
+        if "Picks 24/7" in paragraph.text:
+            if tip_podrske == "24/7":
+                paragraph.text = re.sub(r"Picks 24/7", "", paragraph.text).strip()
+            else:
+                paragraph.text = ""
+        if "Picks 8/5" in paragraph.text:
+            if tip_podrske == "8/5":
+                paragraph.text = re.sub(r"Picks 8/5", "", paragraph.text).strip()
+            else:
+                paragraph.text = ""
+
+    cena_eur = float(extract_cena_eur(st.session_state.uploaded_file_content))
+    broj_meseci = int(broj_meseci)
+    count = 0
+    for paragraph in doc.paragraphs:
+        if "0,00 €" in paragraph.text:
+            if count == 0:
+                paragraph.text = paragraph.text.replace("0,00", str(cena_eur * broj_meseci), 1)
+                paragraph.text = paragraph.text.replace("0,00", str(cena_eur * broj_meseci * 1.2), 1)
+
+                count += 1
+            if count == 1:
+                paragraph.text = paragraph.text.replace("0,00", str(cena_eur), 1)
+                paragraph.text = paragraph.text.replace("0,00", str(cena_eur * 1.2), 1)            
+    doc.save(docx_path.replace('.docx', '_modified.docx'))
 
 def remove_paragraph(paragraph):
     p = paragraph._element
@@ -623,7 +743,7 @@ def main():
 
     
 
-    if st.session_state.uploaded_file_content is not None:
+    if st.session_state.uploaded_file_content:
         new_content = extract_MS_365(st.session_state.uploaded_file_content) + " (radnim danima od 8 do 16):"
         lines = new_content.split("\n")
         non_empty_lines = [line for line in lines if line.strip() != ""]
@@ -639,6 +759,9 @@ def main():
         elif "ESS" in ponuda.name:
             replace_content_ESS(string_without_empty_lines)
             docx_path = r'C:\Users\nemanja.perunicic\OneDrive - Positive doo\Desktop\allIn1\crewai\ugovor ESS_modified.docx'
+        elif "SMS" in ponuda.name:
+            replace_content_SMS(string_without_empty_lines)
+            docx_path = r'C:\Users\nemanja.perunicic\OneDrive - Positive doo\Desktop\allIn1\crewai\ugovor SMS_modified.docx'
 
         # Example usage
         generate_document(docx_path, docx_path.replace('.docx', '_output.docx'), string_without_empty_lines.split('\n'))
@@ -648,6 +771,8 @@ def main():
             os.remove("ugovor PCRM_modified.docx")
         elif "ESS" in ponuda.name:
             os.remove("ugovor ESS_modified.docx")
+        elif "SMS" in ponuda.name:
+            os.remove("ugovor SMS_modified.docx")
 
         st.info("Ugovor je generisan")
         st.divider()
